b627c11682af84653857d1d87a4d558384b9d1e8
Gregory Kelleher <gregory@gregorykelleher.com>
2025-07-09 09:45:20 +0200

    update logging (add file stuff)

--- packages/equity-aggregator/src/equity_aggregator/logging_config.py
+++ packages/equity-aggregator/src/equity_aggregator/logging_config.py
@@ -2,16 +2,18 @@
 
 import logging.config
 import os
+from datetime import date
+
+# get log directory or default
+LOG_DIR = os.getenv("LOG_DIR", "./data/logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# construct log file path
+LOG_FILE = os.path.join(LOG_DIR, f"equity_aggregator_{date.today():%Y-%m-%d}.log")
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {
-        "standard": {
-            "format": "%(asctime)s | %(module)-20s | %(levelname)-5s | %(message)s",
-            "datefmt": "%Y-%m-%dT%H:%M:%S",
-        },
-    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
@@ -19,17 +21,30 @@ LOGGING = {
             "formatter": "standard",
             "stream": "ext://sys.stdout",
         },
+        "file": {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": "standard",
+            "filename": LOG_FILE,
+            "encoding": "utf8",
+        },
+    },
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s | %(module)-20s | %(levelname)-5s | %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S",
+        },
     },
     "loggers": {
         "equity_aggregator": {
             "level": "INFO",
-            "handlers": ["console"],
+            "handlers": ["console", "file"],
             "propagate": False,
         },
     },
     "root": {
         "level": "WARNING",
-        "handlers": ["console"],
+        "handlers": ["console", "file"],
     },
 }
 
@@ -63,8 +78,9 @@ def configure_logging() -> None:
     }
     log_level_selected = level_map.get(env, "INFO")
 
-    # set both root logger and console handler to that level
-    config["loggers"]["equity_aggregator"]["level"] = log_level_selected
+    # set equity_aggregator logger, console and file handler to selected level
+    config["handlers"]["file"]["level"] = log_level_selected
     config["handlers"]["console"]["level"] = log_level_selected
+    config["loggers"]["equity_aggregator"]["level"] = log_level_selected
 
     logging.config.dictConfig(config)
